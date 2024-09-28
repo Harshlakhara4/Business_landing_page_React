9f28e67e487a6541827f2865934a889f017e6265
shubham <lakharaharsh4@gmail.com>
2024-09-28 23:33:40 +0530

    Header

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ className, href, children, onclick, px, white }) => {
  const spanClasses = "relative z-10"
 
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onclick}>
       <span className={spanClasses}> {children}</span>
       {ButtonSvg(white)}
     </button>
